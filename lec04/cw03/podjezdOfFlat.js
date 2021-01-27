f9172adb92912df59c833538b83b2dcb30a70246
AlexP <alexander.pristavka@gmail.com>
2021-01-27 23:46:45 +0200

    Improve algorithm with better interaction behavior;

--- lec04/cw03/podjezdOfFlat.js
+++ lec04/cw03/podjezdOfFlat.js
@@ -7,9 +7,39 @@
 // При вводе некорректных данных предусмотреть alert.
 // Config должен содержать все єти данніе(этажность, число подъездов,  количество квартир на лестничной площадке )и должен біть не публичнім
 
-const RE_INTEGER = /^\d+$/;
+main();
 
+// ENTRY POINT
+function main() {
+  let isCreateNewBuilding = true;
+  while ( isCreateNewBuilding ) {
+    let isAnotherSearch = true;
+    const findEntrance = createEntranceFinder();
+    if ( findEntrance == null ) {
+      return;    }
+    while( isAnotherSearch ) {
+      const flat = askNumberInRange( 'введите номер квартиры, чтобы узнать ее подъезд', 0, Number.MAX_SAFE_INTEGER );
+      if ( flat == null ){
+        return;
+      }
+      const entrance = findEntrance( flat );
+
+      if ( entrance == null ) {
+        alert( `В доме нет квартиры №${flat}` );
+      }
+      else {
+        alert( `Квартира №${flat} находится в подъезде № ${entrance}` );
+      }
+
+      isAnotherSearch = confirm( 'Найти подъезд для другой квартиры?' );
+    }
+    isCreateNewBuilding = confirm( 'Поискать подъезды по квартире в другом доме?' );
+  }
+}
+
+// IMPLEMENTATION ROUTINE
 function askNumberInRange( text, min, max ) {
+  const reInteger = /^\d+$/;
   const toStr = Object.prototype.toString;
   let message = text.trim();
   if( !( toStr.call( message ) === '[object String]' ) || message === '' ) {
@@ -30,9 +60,14 @@ function askNumberInRange( text, min, max ) {
     }
 
     error = '';
-    let result = prompt ( message ).trim();
+    let answer = prompt( message );
+    if ( answer == null ) {
+      return;   // abort dialog;
+    }
 
-    if ( !RE_INTEGER.test( result ) ) {
+    let result = answer.trim();
+
+    if ( !reInteger.test( result ) ) {
       error = 'Ожидается положительное целое число.';
       continue;
     }
@@ -49,51 +84,45 @@ function askNumberInRange( text, min, max ) {
   } while ( error != '' ) 
 
   return number;
-
 }
 
-function createConfig() {
-  return {
-    storeysNum       : askNumberInRange( 'введите количество этажей(1-25)', 1, 25 ),
-    entranceNum      : askNumberInRange( 'введите количество подъездов(1-10)', 1, 10 ),
-    vestibuleFlatNum : askNumberInRange( 'введите количество квартир на лесничной площадке(1-20)', 1, 20 )
-  };
-}
+function getEntranceOfFlat( storeysNum, entranceNum, vestibuleFlatNum, targetFlat ) {
 
-function askTargetFlat() {
-  return askNumberInRange( 'введите номер квартиры, чтобы узнать ее подъезд', 0, Number.MAX_SAFE_INTEGER )
-}
+  const min = 1;
+  const flatsPerEntrance = storeysNum * vestibuleFlatNum;
 
-
-( function( config, flat ) {
-
-  function getEntranceOfFlat( {storeysNum, entranceNum, vestibuleFlatNum}, targetFlat ) {
-
-    const min = 1;
-    const flatsPerEntrance = storeysNum * vestibuleFlatNum;
-
-    var maxFlatNumber =  flatsPerEntrance * entranceNum;
-
-    if ( targetFlat < min || targetFlat > maxFlatNumber ) {
-      // return 'Нет такой квартиры в доме';
-      return null;
-    }
-
-    for( let currentEntrance = min; currentEntrance <= entranceNum; currentEntrance++ ) {
-      let max = flatsPerEntrance * currentEntrance;
-      if ( targetFlat <= max ) {
-        return currentEntrance;
-      }
-    }
-    throw new Error( 'Unexpected error' );
+  var maxFlatNumber =  flatsPerEntrance * entranceNum;
+  if ( targetFlat < min || targetFlat > maxFlatNumber ) {
+    return null;
   }
+
+  for( let currentEntrance = min; currentEntrance <= entranceNum; currentEntrance++ ) {
+    let max = flatsPerEntrance * currentEntrance;
+    if ( targetFlat <= max ) {
+      return currentEntrance;
+    }
+  }
+  throw new Error( 'Unexpected error' );
+}
   
-  const entrance = getEntranceOfFlat( config, flat );
-  if ( entrance == null ) {
-    alert( `В доме нет квартиры №${flat}` );
+
+function createEntranceFinder() {
+  let config = {};
+  config.storeysNum =  askNumberInRange( 'введите количество этажей(1-25)', 1, 25 );
+  if ( config.storeysNum == null ) {
+    return null; // abort dialog;
   }
-  else {
-    alert( `Квартира №${flat} находится в подъезде № ${entrance}` );
+  config.entranceNum =  askNumberInRange( 'введите количество подъездов(1-10)', 1, 10 );
+  if ( config.entranceNum == null ) {
+    return null;
+  }
+  config.vestibuleFlatNum =  askNumberInRange( 'введите количество квартир на лесничной площадке(1-20)', 1, 20 );
+  if ( config.vestibuleFlatNum == null ) {
+    return null;
   }
 
-})( createConfig(), askTargetFlat() );
+  return function ( flat ) {
+    const {storeysNum, entranceNum, vestibuleFlatNum} = config;
+    return getEntranceOfFlat( storeysNum, entranceNum, vestibuleFlatNum, flat );   
+  }
+}
