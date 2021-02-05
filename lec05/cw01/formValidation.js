446793cc078c7c7aee923d986666755e8219552e
AlexP <alexander.pristavka@gmail.com>
2021-02-05 23:46:13 +0200

    Extract values of form fields;

--- lec05/cw01/formValidation.js
+++ lec05/cw01/formValidation.js
@@ -1,3 +1,17 @@
 function validate(){
-  console.log('validate');
+  
+  console.log('validate', extractData() );
+}
+
+function getValueOfInput( id ) {
+  return false;
+}
+
+function extractData() {
+  const inputs = document.querySelectorAll( '.setup input' );
+  let result = {};
+  for ( const field of inputs ) {
+    result[field.name] = field.value;
+  }
+  return result;
 }
