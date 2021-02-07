99eeb70567e82b9a7d339b495ff2d46f025bbef7
AlexP <alexander.pristavka@gmail.com>
2021-02-08 00:35:56 +0200

    Add basic function templates;

--- lec05/hw01/formCreation.js
+++ lec05/hw01/formCreation.js
@@ -1,3 +1,32 @@
-function createForm() {
+function createLayout() {
   
 }
+
+function createToggleWrapper( cls, text ) {
+  
+}
+
+function createButton( cls, text ) {
+}
+
+function createSubmitButton( cls, text ) {
+}
+
+function createHeader( cls, text ) {
+  
+}
+
+function createForm( cls, text ) {
+  
+}
+
+function createFieldContainer( cls, text ) {
+  
+}
+
+function createEl( name, config ) {
+  
+}
+
+
+// TODO: add event listeners and validation functions
