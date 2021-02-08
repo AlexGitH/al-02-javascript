8970507c3dd3a72f6b805f5375da63c9ee7acbf8
AlexP <alexander.pristavka@gmail.com>
2021-02-08 15:10:38 +0200

    Create js layout implementation;

--- lec05/hw01/formCreation.js
+++ lec05/hw01/formCreation.js
@@ -1,32 +1,119 @@
-function createLayout() {
-  
+window.onload = function() {
+  createLayout();
 }
 
-function createToggleWrapper( cls, text ) {
-  
+const fieldContainers = [{
+  name : 'firstName',
+  type : 'text',
+  placeholder: 'First Name *',
+  isLong : false
+},{
+  name : 'lastName',
+  type : 'text',
+  placeholder: 'Last Name *',
+  isLong : false
+},{
+  name : 'email',
+  type : 'text',
+  placeholder: 'Email Address *',
+  isLong : true
+},{
+  name : 'password',
+  type : 'password',
+  placeholder: 'Set A Password *',
+  isLong : true
+}];
+
+function createLayout() {
+  let entry = createMainEl();
+  let formToggle = createFormToggle();
+  const signUp = createButton( 'sign-up', 'Sign Up' );
+  const logIn = createButton( 'log-in', 'Log In' );
+  formToggle.append( signUp, logIn );
+  const heading = createHeader( 'Sign Up for Free' );
+  let form = createForm();
+  for( const config of fieldContainers ) {
+    const { name, type, placeholder, isLong } = config;
+    const field = createFieldContainer( name, type, placeholder, isLong );
+    form.append( field );
+  }
+  const submit = createSubmitButton( 'GET STARTED' ); 
+  form.append( submit );
+  entry.append( formToggle, heading, form );
+  document.body.append( entry );
+}
+
+function createMainEl() {
+  return createEl( 'div', {
+    class: 'entry'
+  });
+}
+
+function createFormToggle() {
+  return createEl( 'div', {
+    class: 'form-toggle'
+  });
 }
 
 function createButton( cls, text ) {
+  let el = createEl( 'button', {
+    class: cls
+  });
+  el.innerText = text;
+  return el;
 }
 
-function createSubmitButton( cls, text ) {
+function createSubmitButton( text ) {
+  let el = createEl( 'button', {
+    class: 'sign-up',
+    type: 'button'
+  });
+  el.innerText = text.toUpperCase();
+  return el;
 }
 
-function createHeader( cls, text ) {
-  
+function createHeader( text ) {
+  let el = createEl( 'h1', {} );
+  el.innerText = text;
+  return el;
 }
 
-function createForm( cls, text ) {
-  
+function createForm() {
+  let el = createEl( 'form', {
+    class: 'frm-sign-up',
+    name: 'signUp'
+  });
+  return el;
 }
 
-function createFieldContainer( cls, text ) {
-  
+function createFieldContainer( name, type, placeholder, isLong ) {
+
+  let container = createEl( 'div', {
+    class : `field-container ${isLong === true ? ' long' : ''}`
+  });
+  let input = createEl( 'input', {
+    id : name,
+    name : name,
+    type : type,
+    placeholder: placeholder
+  });
+  let label = createEl( 'label', {
+    for : name
+  });
+  let div = createEl( 'label', {
+    class : 'valid-info'
+  });
+
+  container.append( input, label, div );
+  return container;
 }
 
 function createEl( name, config ) {
-  
+  let el = document.createElement( name );
+  for( const [attr,val] of Object.entries( config ) ) {
+    el.setAttribute( attr, val );
+  }
+  return el;
 }
-
 
 // TODO: add event listeners and validation functions
